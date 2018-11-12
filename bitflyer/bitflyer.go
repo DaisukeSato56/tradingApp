c8c85ddf521ddfe4e4bc36da35fc275b343d6a54
佐藤大介 <workshift56@gmail.com>
2018-11-12 17:31:00 +0900

    Tickerを用いて、BitCointの情報を取得

--- bitflyer/bitflyer.go
+++ bitflyer/bitflyer.go
@@ -22,11 +22,19 @@ type APIClient struct {
 
 const baseURL = "https://api.bitflyer.com/v1/"
 
+// constructorの生成
+func New(key, secret string) *APIClient {
+	apiClient := &APIClient{key, secret, &http.Client{}}
+	return apiClient
+}
+
 func (api APIClient) header(method, endpoint string, body []byte) map[string]string {
+	// 最初の10桁だけを使う
 	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
 	log.Println(timestamp)
 	message := timestamp + method + endpoint + string(body)
 
+	// hmacに関する知識の復習
 	mac := hmac.New(sha256.New, []byte(api.secret))
 	mac.Write([]byte(message))
 	sign := hex.EncodeToString(mac.Sum(nil))
@@ -39,6 +47,7 @@ func (api APIClient) header(method, endpoint string, body []byte) map[string]str
 }
 
 func (api *APIClient) doRequest(method, urlPath string, query map[string]string, data []byte) (body []byte, err error) {
+	// 正しいURLかどうかを確認
 	baseURL, err := url.Parse(baseURL)
 	if err != nil {
 		return
@@ -47,21 +56,29 @@ func (api *APIClient) doRequest(method, urlPath string, query map[string]string,
 	if err != nil {
 		return
 	}
+
+	// 接続するURLを生成
 	endpoint := baseURL.ResolveReference(apiURL).String()
 	log.Printf("action=doRequest endpoint=%s", endpoint)
+
+	// requstを生成。bytes.NewBuffer(data)はPOSTの際に使用
 	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(data))
 	if err != nil {
 		return
 	}
+
+	// queryの処理
 	q := req.URL.Query()
 	for key, value := range query {
 		q.Add(key, value)
 	}
 	req.URL.RawQuery = q.Encode()
 
+	// 上で定義したheader関数を使用
 	for key, value := range api.header(method, req.URL.RequestURI(), data) {
 		req.Header.Add(key, value)
 	}
+
 	resp, err := api.httpClient.Do(req)
 	if err != nil {
 		return nil, err
@@ -88,6 +105,8 @@ func (api *APIClient) GetBalance() ([]Balance, error) {
 		log.Printf("action=GetBalance err=%s", err.Error())
 		return nil, err
 	}
+
+	// responseをunmarshalしてやるために、スライスを作ってあげる
 	var balance []Balance
 	err = json.Unmarshal(resp, &balance)
 	if err != nil {
@@ -95,4 +114,50 @@ func (api *APIClient) GetBalance() ([]Balance, error) {
 		return nil, err
 	}
 	return balance, nil
+}
+
+type Ticker struct {
+	ProductCode     string  `json:"product_code"`
+	Timestamp       string  `json:"timestamp"`
+	TickID          int     `json:"tick_id"`
+	BestBid         float64 `json:"best_bid"`
+	BestAsk         float64 `json:"best_ask"`
+	BestBidSize     float64 `json:"best_bid_size"`
+	BestAskSize     float64 `json:"best_ask_size"`
+	TotalBidDepth   float64 `json:"total_bid_depth"`
+	TotalAskDepth   float64 `json:"total_ask_depth"`
+	Ltp             float64 `json:"ltp"`
+	Volume          float64 `json:"volume"`
+	VolumeByProduct float64 `json:"volume_by_product"`
+}
+
+func (t *Ticker) GetMidPrice() float64 {
+	return (t.BestBid + t.BestAsk) / 2
+}
+
+func (t *Ticker) DateTime() time.Time {
+	dateTime, err := time.Parse(time.RFC3339, t.Timestamp)
+	if err != nil {
+		log.Printf("action=DateTime, err=%s", err.Error())
+	}
+	return dateTime
+}
+
+func (t *Ticker) TruncateDateTime(duration time.Duration) time.Time {
+	return t.DateTime().Truncate(duration)
+}
+
+func (api *APIClient) GetTicker(productCode string) (*Ticker, error) {
+	url := "ticker"
+	resp, err := api.doRequest("GET", url, map[string]string{"product_code": productCode}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var ticker Ticker
+	err = json.Unmarshal(resp, &ticker)
+	if err != nil {
+		return nil, err
+	}
+	return &ticker, nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
-	"gotrading_application/110_sma/bitflyer"
+	"myApp/tradingApp/bitflyer"
 	"myApp/tradingApp/config"
 	"myApp/tradingApp/utils"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
