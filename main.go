7c7882f2d1b3d47ff93dbcc139bd440beb4f9421
佐藤大介 <workshift56@gmail.com>
2018-11-15 17:26:47 +0900

    webサーバーを立ち上げて、google candle stick chartsを表示

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"myApp/tradingApp/App/controllers"
-	"myApp/tradingApp/App/models"
 	"myApp/tradingApp/config"
 	"myApp/tradingApp/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DBConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
